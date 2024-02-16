84ec772073b89ef39f02db098e751e1428866ba4
albertobarbano <albertobarbanog@gmail.com>
2024-02-16 14:54:45 -0300

    Added email notification functionality with attached image ('image.png') for new customer detection.

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import cv2
 import time
 from emailing import send_email
+import glob
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
 
 first_frame = None
 status_list = []
+
+count = 1
 
 while True:
     status = 0
@@ -32,12 +35,18 @@ while True:
         rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
         if rectangle.any():
             status = 1
+            cv2.imwrite(f"images/{count}.png", frame)
+            count = count + 1
+            all_images = glob.glob("images/*.png")
+            index = int(len(all_images)/2)
+            images_with_object = all_images[index]
+
 
     status_list.append(status)
     status_list = status_list[-2:]
 
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email()
+        send_email(images_with_object)
 
     print(status_list)
 
